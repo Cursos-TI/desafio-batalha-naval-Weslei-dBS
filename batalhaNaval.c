350f7ffdd1773fbb4ceb320a0f9f5a347a1e708d
Weslei de Brito Silva <weslei.dbsilva@gmail.com>
2025-03-26 19:03:44 +0000

    Desafio Aventureiro COMPLETO

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,35 +1,62 @@
 #include <stdio.h>
 
+#define TAMANHO 10
+#define TAM_NAVIO 3
+
+
 int main() {
     
-    char linha[10] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    int Tabuleiro [10][10] = {0};
+    char Head[10] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int Tabuleiro [TAMANHO][TAMANHO] = {0};
+    int Soma = 'i' + 'j';
 
     //Navio na Vertical B2, B3 e B4.
-    Tabuleiro [1][1] = 3;
-    Tabuleiro [2][1] = 3;
-    Tabuleiro [3][1] = 3;
+
+    for (int i = 0; i < TAM_NAVIO; i++)
+    {
+        Tabuleiro[1+i][1] = 3;
+    }
+    
 
     //Navio na Horizontal F3, G3 e H3.
-    Tabuleiro [2][5] = 3;
-    Tabuleiro [2][6] = 3;
-    Tabuleiro [2][7] = 3;
 
-    printf("   *TABULEIRO BATALHA NAVAL* \n  \n");
+    for (int i = 0; i < TAM_NAVIO; i++)
+    {
+        Tabuleiro[2][5+i] = 3;
+    }
+
+    //Navio na Diagonal C3,D4,E5
+
+    for (int i = 0; i < TAM_NAVIO; i++)
+    {
+        Tabuleiro[2+i][2+i] = 3;
+    }
+
+    //Navio na Diagonal Invertida J8,I9,H10
+
+    for (int i = 0; i < TAM_NAVIO; i++)
+    {   
+        Tabuleiro[9-i][7+i] = 3;
+    }
+    
+
+    
+
+    printf("   *TABULEIRO BATALHA NAVAL* \n\n");
     printf("   "); // Espaçamento para alinhar as colunas com a variável "linha"
 
     //cabeçalho
     for (int j = 0; j < 10; j++)
     {
-        printf(" %c ", linha[j]);
+        printf(" %c ", Head[j]);
     }
     printf("\n");
 
     //Exibição do Tabuleiro
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < TAMANHO; i++)
     {
         printf("%2d ", i + 1); // Adicionando uma coluna de indice numerica.
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < TAMANHO; j++)
         {
             printf(" %d ", Tabuleiro[i][j]);
         }
